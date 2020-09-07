229241ab24d03d70247147216fc3ba3aa1d779b8
Andrey131 <mulitza.an@mail.ru>
2020-09-07 22:16:31 +0300

    Init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,197 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+
+class BarBodyImage extends React.Component {
+
+  render() {
+    return (
+      <article tabindex="-1" className={this.props.ClassName} onClick = {this.props.onClick}>
+        <a className="barBodyImage" href={this.props.ImageHref} data-position="left center">
+          <img src={this.props.ImageSrc} alt />
+        </a>
+      </article>
+    );
+  }
+
+}
+
+
+class BarHead extends React.Component {
+  render() {
+    return (
+      <div className="barHead">
+        <h1>Lens</h1>
+        <p>
+          Just another fine responsive site template by 
+          <a href="https://html5up.net/">HTML5 UP</a>
+        </p>
+        <ul className="icons">
+          <li>
+            <a href="#" className="icon twitter">
+              <span className="label">Twitter</span>
+            </a>
+          </li>
+
+
+          <li>
+            <a href="#" className="icon instagram">
+              <span className="label">Instagram</span>
+            </a>
+          </li>
+
+
+          <li>
+            <a href="#" className="icon github">
+              <span className="label">Github</span>
+            </a>
+          </li>
+
+
+          <li>
+            <a href="#" className="icon email">
+              <span className="label">Email</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+
+class BarBody extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.imageChanger = this.imageChanger.bind(this);
+    this.state = {
+      images: Array(12).fill(null),
+    };
+
+    for(let i=1; i<=12; i++){
+ 
+     this.state.images[i-1]={
+        ImageSrc: 'images/mini/'+i+'.jpg',
+        ImageHref: 'images/fulls/'+i+'.jpg',
+        ImageActive:  (i==1)?"active":undefined,
+        onClick: () => this.imageChanger(i)
+        }
+    }
+  }
+
+  imageChanger(i){
+    const images = this.state.images;
+
+    if(images[i-1].ImageActive=='active'){
+    }
+
+    else{
+
+     for(let j=0; j<images.length; j++){
+       if(images[j].ImageActive=="active"){
+        images[j].ImageActive=undefined;
+       }
+     }
+
+     images[i-1].ImageActive='active';
+     document.getElementsByClassName("bigImage")[0].style.backgroundImage = "url(./images/fulls/"+i+".jpg)";
+     this.setState({
+       images: images
+     });
+    }
+  }
+
+  render() {
+    return (
+      <section className="barBody">
+        {this.state.images.map((item)=>{
+          return (
+            <BarBodyImage 
+              ImageSrc={item.ImageSrc} 
+              ClassName={item.ImageActive} 
+              onClick={item.onClick}/>)
+        })}
+      </section>
+    );
+  }
+
+}
+
+
+class BarFoot extends React.Component {
+  render() {
+    return (
+      <div className="barFoot">
+        <ul>
+          <li>© Untitled.</li>
+          <li>Design: <a href="https://html5up.net">HTML5 UP</a>.</li>
+          <li>Images: <a href="https://unsplash.com">Unsolash</a>.</li>
+        </ul>
+      </div>
+    );
+  }
+
+}
+
+
+
+class Bar extends React.Component {
+  render() {
+    return (
+      <div className="bar">
+        <BarHead />
+        <BarBody />
+        <BarFoot />
+      </div>
+    );
+  }
+
+}
+
+
+class View extends React.Component {
+  render() {
+    return (
+      <div className="view">
+        <div className="nav">
+          <div className="nav-next"></div>
+          <div className="nav-previous"></div>
+          <div className="toggle"></div>
+        </div>
+
+        <div className="activeImage">
+          <div className="caption">
+            <h2>Diam tempus accumsan</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+          <div className="bigImage">
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Bar />
+       <View /> 
+      </div>
+    );
+  }
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
